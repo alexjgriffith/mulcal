398e4feecc804e084d61a8b36763902595caccbe
alexjgriffith <griffitaj@gmail.com>
2015-02-08 17:44:42 -0500

    before trimming

--- R/test.r
+++ R/test.r
@@ -1,15 +1,23 @@
-library(roxygen2)
+#!/data/binaries/R-3.1.0/bin/Rscript
+#
+# This file is part of peakAnalysis,
+# http://github.com/alexjgriffith/alpha-score/, 
+# and is Copyright (C) University of Ottawa, 2015. It is Licensed under 
+# the three-clause BSD License; see LICENSE.txt.
+# Author : Alexander Griffith
+# Contact: griffitaj@gmail.com
+#
+#
+######################################################################
+######################################################################
 
-#' Test function
-#'
-#' This function is to test the roxygen2 markup language
-#' @param  inputList
-#' @export
-#' @author Alexander Griffith <griffitaj@@gmail.com>
-#' @examples
-#' testFun(c(1,2,3,4))
-testFun<-function(inputList)
-    {
-        for( i in inputList)
-            print(i)
-    }
+loadProject<-function(projectDirectory,files){
+    tempDir<-getwd()
+    args <- commandArgs(trailingOnly = F)
+    scriptDir<-dirname(sub("--file=","",args[grep("--file",args)]))
+    setwd(scriptDir)
+    for(file in files){
+        source(paste(projectDirectory,file,sep=""),chdir=TRUE)}
+    setwd(tempDir)}
+
+loadProject("../src/r/", c("pcaAnalysis.r","html.r"))
